cbfff20b8f56adaec6425470fb54abb95280cda5
hyeran1122 <97385586+hyeran1122@users.noreply.github.com>
2024-01-05 16:27:01 +0900

    로그인 후 화면 연결 오류해결

--- instaClone/instaClone/LoginViewController.swift
+++ instaClone/instaClone/LoginViewController.swift
@@ -51,7 +51,7 @@ class LoginViewController: UIViewController {
         
         if userInfo.email == self.email
             && userInfo.password == self.password {
-            let vc = storyboard?.instantiateViewController(withIdentifier: "TextVC") as! UITabBarController
+            let vc = storyboard?.instantiateViewController(withIdentifier: "TabBarVC") as! UITabBarController
             vc.modalPresentationStyle = .fullScreen
             self.present(vc, animated: true, completion: nil)
         }
