012d24a6f6025dabc9695a633081a6bd5724fe00
hyeran1122 <97385586+hyeran1122@users.noreply.github.com>
2024-01-11 20:02:21 +0900

    피드 레이아이웃

---
+++ instaClone/instaClone/Home/Cell/FeedTableViewCell.swift
@@ -0,0 +1,63 @@
+//
+//  FeedTableViewCell.swift
+//  instaClone
+//
+//  Created by 김혜란 on 1/9/24.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageViewUserProfile: UIImageView!
+    @IBOutlet weak var labelUserName: UILabel!
+    @IBOutlet weak var imageViewFeed: UIImageView!
+    @IBOutlet weak var buttonIsHeart: UIButton!
+    @IBOutlet weak var buttonIsBookMark: UIButton!
+    @IBOutlet weak var labelHowNanyLike: UILabel!
+    @IBOutlet weak var labelFeed: UILabel!
+    @IBOutlet weak var imageViewMyProfile: UIImageView!
+    
+    
+    @IBAction func actionIsHeart(_ sender: Any) {
+        if buttonIsHeart.isSelected {
+            buttonIsHeart.isSelected = false
+        } else {
+            buttonIsHeart.isSelected = true
+        }
+    }
+    
+    @IBAction func actionButtonMark(_ sender: Any) {
+        if buttonIsBookMark.isSelected {
+            buttonIsBookMark.isSelected = false
+        } else {
+            buttonIsBookMark.isSelected = true
+        }
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        imageViewUserProfile.layer.cornerRadius = 22.5
+        imageViewUserProfile.clipsToBounds = true           //코너라운드 줬을 떄 안에 이미지도 같이 잘리게 하기
+        
+        imageViewMyProfile.layer.cornerRadius = 12.5        //코너레디우스 보다 작게 넣는건가?
+        imageViewMyProfile.clipsToBounds = true
+        
+        //커스텀 라벨
+        let fontSize = UIFont.boldSystemFont(ofSize: 9)
+        let attributerStr = NSMutableAttributedString(string: labelFeed.text ?? "")
+        attributerStr.addAttribute(.font, value : fontSize, range: NSRange.init(location: 0, length: 3))
+        
+        labelFeed.attributedText = attributerStr
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- instaClone/instaClone/Home/HomeViewController.swift
+++ instaClone/instaClone/Home/HomeViewController.swift
@@ -18,6 +18,14 @@ class HomeViewController: UIViewController {
         //테이블 뷰의 델리게이트 = 나다 = 테이블뷰에 나를 띄워라, 데이터 소스도 = 나다
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.separatorStyle = .none        //클릭했을 때 색 반전 없앰
+        
+        
+        //테이블 뷰에 등록하는 것
+        //닙을 사용해서 아이텐티 파이어를 넣고 변수를 선언해주고
+        let feedNib = UINib(nibName: "FeedTableViewCell", bundle: nil)
+        //해당 변수를 테이블 뷰에 등록해준다.
+        tableView.register(feedNib, forCellReuseIdentifier: "FeedTableViewCell")
 
     }
     
@@ -40,8 +48,17 @@ extension HomeViewController : UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.backgroundColor = .blue
+        //디큐 =. 재사용 셀
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FeedTableViewCell", for: indexPath) as? FeedTableViewCell
+        else {
+            return UITableViewCell()
+        }
+        cell.selectionStyle = .none //테이블별 구분선 없앰
         return cell
+    }
+    
+    //테이블뷰 높이 설정
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 600
     }
 }
