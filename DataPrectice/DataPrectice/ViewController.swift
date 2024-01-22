be220f99b097c01f087fe32b2f5713f84656e970
hyeran1122 <97385586+hyeran1122@users.noreply.github.com>
2024-01-22 23:34:27 +0900

    수정,삭제 기능추가

--- DataPrectice/DataPrectice/ViewController.swift
+++ DataPrectice/DataPrectice/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "CoreData To Do Lsit"
-        getAllItems()
+        getAllItems()       //뷰디드로드할 때 아이템들 다 가지고 오기
         view.addSubview(tableView)
         tableView.delegate =  self
         tableView.dataSource =  self
@@ -33,7 +33,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     @objc private func didTapAdd() {
-        let alert = UIAlertController(title: "New Item", 
+        let alert = UIAlertController(title: "New Item",
                                       message: "Enter new item",
                                       preferredStyle: .alert)
         
@@ -59,6 +59,47 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.textLabel?.text = model.name
         return cell
     }
+    
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let item = models[indexPath.row]
+        let sheet = UIAlertController(title: "Edit",
+                                      message: nil,
+                                      preferredStyle: .actionSheet)
+        
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil ))
+        
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { _ in
+            let alert = UIAlertController(title: "Edit Item",
+                                          message: "Edit your maeeage",
+                                          preferredStyle: .alert)
+            
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            alert.addAction(UIAlertAction(title: "Save", style: .cancel, handler: { [weak self] _ in
+                guard let field = alert.textFields?.first, let newName = field.text, !newName.isEmpty else {
+                    return
+                }
+                
+                self?.updateItem(item: item, newName: newName)
+            }))
+            
+            self.present(sheet, animated: true)
+            
+        }))
+        
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak self] _ in
+            self?.deleteItem(item: item)
+        }))
+        
+        present(sheet, animated: true)
+        
+        
+    }
+    
     
     //Core Data
     func getAllItems() {
