5729c00ccf4dea94bff413f2cc9afe5f13bd62cb
hyeran1122 <97385586+hyeran1122@users.noreply.github.com>
2024-01-22 22:49:59 +0900

    네비게이션 연결

--- DataPrectice/DataPrectice/ViewController.swift
+++ DataPrectice/DataPrectice/ViewController.swift
@@ -7,18 +7,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+    
+    private var models = [ToDoListItem]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "CoreData To Do Lsit"
+        getAllItems()
+        view.addSubview(tableView)
+        tableView.delegate =  self
+        tableView.dataSource =  self
+        tableView.frame = view.bounds
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
     }
-
+    
+    @objc private func didTapAdd() {
+        let alert = UIAlertController(title: "New Item", message: "Enter new item", preferredStyle: .alert)
+        
+        alert.addTextField(configurationHandler: nil)
+        
+        alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: {[weak self] _ in
+            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else {
+                return
+            }
+            
+            self?.createItem(name: text)
+        }))
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model.name
+        return cell
+    }
+    
+    //Core Data
     func getAllItems() {
         do {
-            let items = try context.fetch(ToDoListItem.fetchRequest())
+            models = try context.fetch(ToDoListItem.fetchRequest())
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
         catch {
             //error
@@ -32,6 +77,7 @@ class ViewController: UIViewController {
         
         do {
             try context.save()
+            getAllItems()
         }
         catch{
             
