28940b8f0b72b81e535188e794c1b35691bac02c
hyeran1122 <97385586+hyeran1122@users.noreply.github.com>
2024-01-29 20:47:56 +0900

    MamberList 파일 생성 / 테이블 뷰 생성

---
+++ MemberList/MemberList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemberList
+//
+//  Created by 김혜란 on 1/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
